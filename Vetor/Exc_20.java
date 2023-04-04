be74007b76d31264cdb381868d539579d54353c3
VINICIUS200023 <125417122+VINICIUS200023@users.noreply.github.com>
2023-04-04 14:07:58 -0300

    Create Exc_20.java

---
+++ Vetor/Exc_20.java
@@ -0,0 +1,13 @@
+/*Implementar um programa que obtenha a cotação do dólar (U$) em 
+relação ao real (R$) e a seguir armazene em vetor A com 20 
+elementos as seguintes conversões */
+
+package Vetor;
+
+public class Exc_20 {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
