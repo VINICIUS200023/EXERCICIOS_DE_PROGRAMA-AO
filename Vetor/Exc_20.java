868b5179653dc033483ff43850076ac6f3cab224
VINICIUS200023 <125417122+VINICIUS200023@users.noreply.github.com>
2023-04-08 07:20:09 -0300

    Update Exc_20.java

--- Vetor/Exc_20.java
+++ Vetor/Exc_20.java
@@ -4,10 +4,20 @@ elementos as seguintes conversões */
 
 package Vetor;
 
+import java.util.Scanner;
+
 public class Exc_20 {
 
     public static void main(String[] args) {
-        
+
+        Scanner le = new Scanner(System.in);
+
+        double[] vetorA = new double[20];
+        double dolar = 5.08;
+
+        for (int i = 0; i < vetorA.length; i++) {
+            System.out.println("INFORME O VALOR DO VETOR " + (i + 1));
+            vetorA[i] = le.nextDouble();
+        }
     }
-    
 }
